5f7d88f5665a4924be5756c210b6bdb2804e3942
George <fatnev@yandex.ru>
2024-07-28 21:04:50 +0300

    Update

--- include.php
+++ include.php
@@ -1,25 +1,2 @@
 <?php
-
-// Подключение файла настроек модуля
-IncludeModuleLangFile(__FILE__);
-
-$arModuleClasses = array(
-    // Ваши классы модуля тут, если есть
-);
-
-CModule::AddAutoloadClasses("faq", $arModuleClasses);
-
-if (IsModuleInstalled("faq")) {
-    global $APPLICATION;
-    $APPLICATION->AddPanelButton(
-        array(
-            "ID" => "faq_module_button",
-            "TEXT" => "Управление FAQ",
-            "MAIN_SORT" => 10000,
-            "SORT" => 10,
-            "HREF" => "/bitrix/admin/settings.php?mid=faq&lang=".LANGUAGE_ID,
-            "ICON" => "bx-panel-site-speed",
-            "TITLE" => "Управление настройки модуля FAQ",
-        )
-    );
-}
+//IncludeModuleLangFile(__FILE__);
